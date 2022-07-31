359918d1568ef53454ea9aa42ec7d9c123487368
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2022-07-31 14:35:38 -0700

    wc: restore main driver program

--- wc/__main__.py
+++ wc/__main__.py
@@ -1,33 +1,27 @@
+from . import args
+from . import log
+
+from .memory.memory import Memory
+from .data.data import Data
+from .event.events import Events
+from .menus.menus import Menus
+from .battle import Battle
+from .settings import Settings
+from .bug_fixes import BugFixes
+
+
 def main():
-    pass
-    """
-    from . import args
-    from . import log
-
-    from .memory.memory import Memory
     memory = Memory()
-
-    from .data.data import Data
     data = Data(memory.rom, args)
 
-    from .event.events import Events
     events = Events(memory.rom, args, data)
-
-    from .menus.menus import Menus
     menus = Menus(data.characters, data.dances)
-
-    from .battle import Battle
     battle = Battle()
-
-    from .settings import Settings
     settings = Settings()
-
-    from .bug_fixes import BugFixes
     bug_fixes = BugFixes()
 
     data.write()
     memory.write()
-    """
 
 if __name__ == '__main__':
     main()
